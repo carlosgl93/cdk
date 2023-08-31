30061a2905a8f3ae7bcd79ab80b3236cea58a11c
Carlos <cgumuciolabbe@gmail.com>
2023-08-31 22:15:18 +0800

    first commit

--- lib/my-first-cdk-stack.ts
+++ lib/my-first-cdk-stack.ts
@@ -1,6 +1,6 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
 export class MyFirstCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,5 +12,13 @@ export class MyFirstCdkStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'MyFirstCdkQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    new lambda.Function(this, "lambdaFunction", {
+      functionName: "first-cdk-lambda",
+      code: new lambda.AssetCode("src"),
+      handler: "index.handler",
+      runtime: lambda.Runtime.NODEJS_18_X,
+      memorySize: 128,
+    });
   }
 }
